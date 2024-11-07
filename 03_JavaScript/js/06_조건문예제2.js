c85cb3bb591eb097a23e97526503c68627887a0f
JGS-JAVA <wlrncom@naver.com>
2024-11-07 11:33:53 +0900

    11.7

--- 03_JavaScript/js/06_조건문예제2.js
+++ 03_JavaScript/js/06_조건문예제2.js
@@ -1,7 +1,12 @@
-if (score >= 60) {
-  msg.innerText = "합격";
-  msg.className = "success";
-} else {
-  msg.innerText = "불합격";
-  msg.className = "fail";
+function checkAge() {
+  const score = document.getElementById("score").value;
+  const msg = document.getElementById("scoreMsg");
+
+  if (score >= 60) {
+    msg.innerText = "합격";
+    msg.className = "success";
+  } else {
+    msg.innerText = "불합격";
+    msg.className = "fail";
+  }
 }
